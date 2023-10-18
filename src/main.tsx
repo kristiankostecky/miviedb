1737b8142709db4d36ae1743d4f43747b3ed8249
Kristián Kostecký <kostecky.kristian@gmail.com>
2023-10-18 16:01:46 +0200

    Small general improvements

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  RouterProvider,
-  createBrowserRouter,
-  useRouteError,
-} from 'react-router-dom'
-import { ValiError } from 'valibot'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ErrorBoundary } from './components/error-boundry'
 import './index.css'
 import { Root } from './routes/root/route'
 import { Search } from './routes/search'
 import { PATHS } from './utils/constants'
-import { ErrorBoundary } from './components/error-boundry'
 
 const queryClient = new QueryClient({
   defaultOptions: {
